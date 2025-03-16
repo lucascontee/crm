829f67bcb4170b1ef9a878059ed2db45900dabee
Lucas Conte <lucascontee@icloud.com>
2025-03-15 22:54:15 -0300

    criando o CostumerController e aplicando seus endpoints para CRUD

--- Controllers/CostumersController.cs
+++ Controllers/CostumersController.cs
@@ -15,12 +15,77 @@ namespace CRM.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Costumer> list = _context.Costumers.ToList();
+            if (list == null)
+            {
+                return NoContent();
+            }
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var costumer = _context.Costumers.FirstOrDefault(c => c.Id == id);
+
+            if (costumer == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(costumer);
+        }
+
         [HttpPost]
-        public IActionResult CreateCostumer(Costumer costumer)
+        public IActionResult Post(Costumer costumer)
         {
             _context.Add(costumer);
             _context.SaveChanges();
             return Ok(costumer);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Costumer costumer)
+        {
+            var costumerDB = _context.Costumers.FirstOrDefault(c => c.Id == id);
+
+            if (costumerDB == null)
+            {
+                return NotFound();
+            }
+
+            costumerDB.StoreName = costumer.StoreName;
+            costumerDB.FirstName = costumer.FirstName;
+            costumerDB.LastName = costumer.LastName;
+            costumerDB.Address = costumer.Address;
+            costumerDB.Region = costumer.Region;
+            costumerDB.Phone = costumer.Phone;
+            costumerDB.MainInformation = costumer.MainInformation;
+
+            _context.Costumers.Update(costumerDB);
+            _context.SaveChanges();
+
+            return Ok(costumerDB);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var costumer = _context.Costumers.FirstOrDefault(c => c.Id == id);
+
+            if(costumer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Costumers.Remove(costumer);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 using CRM.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CRM", Version = "v1" });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -15,6 +22,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
